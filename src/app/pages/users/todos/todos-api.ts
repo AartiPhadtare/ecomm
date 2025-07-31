46a7fec78678632d7dec9604f008bd019cc95985
Sashikumar <yshashi30@gmail.com>
2025-07-31 19:28:02 +0530

    feat: implement Todos API with CRUD operations and update UI for todo management

--- src/app/pages/users/todos/todos-api.ts
+++ src/app/pages/users/todos/todos-api.ts
@@ -1,12 +1,23 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable()
-export class TodosApi{
-    http = inject(HttpClient);
+export class TodosApi {
+  http = inject(HttpClient);
 
-    getTodos(){
-       return this.http.get('https://jsonplaceholder.typicode.com/todos')
-    }
+  getTodos() {
+    return this.http.get('http://localhost:3000/todos');
+  }
 
+  updateTodo(body: any) {
+    return this.http.put(`http://localhost:3000/todos/${body.id}`, body);
+  }
+
+  deleteTodo(id: number) {
+    return this.http.delete(`http://localhost:3000/todos/${id}`);
+  }
+
+  //post ya put karte hoo
+
+  // { email: "", password:"" } {name: "", phone: "", address: "", mail:""}
 }
--- src/app/pages/users/todos/todos.ts
+++ src/app/pages/users/todos/todos.ts
@@ -1,53 +1,122 @@
-import { Component, inject } from "@angular/core";
-import { TodosApi } from "./todos-api";
+import { Component, inject } from '@angular/core';
+import { TodosApi } from './todos-api';
 
 @Component({
-    selector:'app-todos',
-    template:`
-     <div class="container mx-auto w-full max-w-3xl p-6">
-    <table class="min-w-full bg-white border border-gray-300 rounded-lg overflow-hidden shadow-md">
-      <thead class="bg-gray-200 text-gray-700 text-left">
-        <tr>
-          <th class="py-3 px-4 border-b">User ID</th>
-          <th class="py-3 px-4 border-b">ID</th>
-          <th class="py-3 px-4 border-b">Title</th>
-          <th class="py-3 px-4 border-b">Completed</th>
-        </tr>
-      </thead>
-      <tbody>
-        @for(todo of todolist; track todo.id){
-            <tr class="hover:bg-gray-50">
-                <td class="py-3 px-4 border-b">{{todo.id}}</td>
-          <td class="py-3 px-4 border-b">{{todo.userId}}</td>
-          
-          <td class="py-3 px-4 border-b">{{todo.title}}</td>
-          <td class="py-3 px-4 border-b">
-          @if (todo.completed) {
-            <span class="text-green-500 text-xl"><i class="fa fa-check-square" aria-hidden="true"></i></span>
-          }@else {
-            <span class="text-red-500 text-xl"><i class="fa fa-times" aria-hidden="true"></i></span>
-          }
-          </td>
-        </tr>
-        }
-      </tbody>
-    </table>
-  </div>
-    `,
-    providers:[TodosApi]
-})
-export class Todos{
-    todoapi = inject(TodosApi);
-    todolist :any =[];
+  selector: 'app-todos',
+  template: `
+    <div class="container mx-auto w-full max-w-7xl p-6">
+      <div class="flex justify-end">
+        <button
+          class=" bg-cyan-500 p-2 px-4 text-white mb-2  rounded-xl cursor-pointer"
+        >
+          Add Todo
+        </button>
+      </div>
 
-    constructor(){
-       this.todoapi.getTodos().subscribe({
-        next: (res: any) =>{
-            this.todolist = res;
-        },
-        error: (err) =>{
-            console.log(err);
-        }
-       })
-    }
+      <table
+        class="min-w-full bg-white border border-gray-300 rounded-lg overflow-hidden shadow-md"
+      >
+        <thead class="bg-gray-200 text-gray-700 text-left">
+          <tr>
+            <th class="py-3 px-4 border-b">ID</th>
+            <th class="py-3 px-4 border-b">User ID</th>
+
+            <th class="py-3 px-4 border-b">Title</th>
+            <th class="py-3 px-4 border-b">Completed</th>
+            <th class="py-3 px-4 border-b">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for(todo of todoList; track todo.id){
+          <tr class="hover:bg-gray-50">
+            <td class="py-3 px-4 border-b">{{ todo.id }}</td>
+            <td class="py-3 px-4 border-b">{{ todo.userId }}</td>
+
+            <td class="py-3 px-4 border-b">{{ todo.title }}</td>
+            <td class="py-3 px-4 border-b">
+              @if (todo.completed) {
+              <span class="text-green-500 text-xl"
+                ><i class="fa fa-check-square" aria-hidden="true"></i
+              ></span>
+              }@else {
+              <span class="text-red-500 text-xl"
+                ><i class="fa fa-times" aria-hidden="true"></i
+              ></span>
+              }
+            </td>
+            <td class="py-3 px-4 border-b">
+              @if (todo.completed) {
+              <button
+                (click)="changeStatus(todo)"
+                class="p-1 px-4 bg-amber-500 text-xs text-white rounded-2xl cursor-pointer"
+              >
+                Mark as incomplete
+              </button>
+              }@else {
+              <button
+                (click)="changeStatus(todo)"
+                class="p-1 px-4 bg-green-500 text-xs text-white rounded-2xl cursor-pointer"
+              >
+                Mark as complete
+              </button>
+              <button
+                (click)="deleteTodo(todo.id)"
+                class="p-1 px-4 bg-red-500 text-xs ml-3 text-white rounded-2xl cursor-pointer"
+              >
+                Delete
+              </button>
+              }
+            </td>
+          </tr>
+          }
+        </tbody>
+      </table>
+    </div>
+  `,
+  providers: [TodosApi],
+})
+export class Todos {
+  todoApi = inject(TodosApi);
+  todoList: any = [];
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.todoApi.getTodos().subscribe({
+      next: (res: any) => {
+        this.todoList = res;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  changeStatus(todo: any) {
+    todo.completed = !todo.completed;
+    this.todoApi.updateTodo(todo).subscribe({
+      next: (res) => {
+        console.log(res);
+        alert('Todo status changed');
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todoApi.deleteTodo(id).subscribe({
+      next: (res) => {
+        console.log(res);
+        alert('Todo item deleted');
+        this.getTodos();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
